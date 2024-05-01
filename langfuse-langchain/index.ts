2439b88baf8070d7d6fe5d581efe8f178ed64e42
mantellumius <mantellumius@gmail.com>
2024-05-01 14:09:19 +0500

    build

--- langfuse-langchain/index.ts
+++
@@ -1,8 +0,0 @@
-export { Langfuse } from "langfuse";
-
-import { CallbackHandler } from "./src/callback";
-
-export default CallbackHandler;
-
-export { CallbackHandler };
-export * from "./src/callback";
